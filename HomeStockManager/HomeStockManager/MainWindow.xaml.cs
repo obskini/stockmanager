afd6c061f1712eb8a5195392aa38528835251253
obskini <84070185+obskini@users.noreply.github.com>
2024-03-06 08:59:24 +0100

    register check

--- HomeStockManager/HomeStockManager/MainWindow.xaml.cs
+++ HomeStockManager/HomeStockManager/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
 using HomeStockManager.Classes;
 
 namespace HomeStockManager
@@ -49,20 +51,132 @@ namespace HomeStockManager
             string email = RegisterEmailTextBox.Text;
             string password = RegisterPasswordBox.Password;
 
-            if (db.Register(firstName, lastName, username, email, password))
+            bool validUser = CheckUser(firstName, lastName, username, email, password, out string notification);
+
+            if (validUser)
             {
-                MessageBox.Show("Registration successful!");
-                db.InsertNotification(username, "You have registered an account.");
-                RegisterFirstNameTextBox.Clear();
-                RegisterLastNameTextBox.Clear();
-                RegisterUsernameTextBox.Clear();
-                RegisterEmailTextBox.Clear();
-                RegisterPasswordBox.Clear();
+                if (db.Register(firstName, lastName, username, email, password))
+                {
+                    MessageBox.Show("Registration successful!");
+                    RegisterFirstNameTextBox.Clear();
+                    RegisterLastNameTextBox.Clear();
+                    RegisterUsernameTextBox.Clear();
+                    RegisterEmailTextBox.Clear();
+                    RegisterPasswordBox.Clear();
+                }
+                else
+                {
+                    MessageBox.Show("Registration failed!");
+                }
             }
             else
             {
-                MessageBox.Show("Registration failed!");
+                MessageBox.Show($"Error: {notification}.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
             }
+        }
+
+        private bool CheckUser(string firstName, string lastName, string username, string email, string password, out string notification)
+        {
+            bool output = true;
+            List<string> meldingen = new List<string>();
+            notification = "";
+
+            if (string.IsNullOrWhiteSpace(firstName))
+            {
+                meldingen.Add("First name is required.");
+                output = false;
+            }
+            else
+            {
+                if (firstName.Length < 2)
+                {
+                    meldingen.Add("First name should contain at least 2 letters.");
+                    output = false;
+                }
+
+                if (!firstName.All(char.IsLetter))
+                {
+                    meldingen.Add("First name can only contain alphabetic characters.");
+                    output = false;
+                }
+            }
+
+            if (string.IsNullOrWhiteSpace(lastName))
+            {
+                meldingen.Add("Last name is required.");
+                output = false;
+            }
+            else
+            {
+                if (lastName.Length < 2)
+                {
+                    meldingen.Add("Last name should contain at least 2 letters.");
+                    output = false;
+                }
+
+                if (!lastName.All(char.IsLetter))
+                {
+                    meldingen.Add("Last name can only contain alphabetic characters.");
+                    output = false;
+                }
+            }
+
+            if (string.IsNullOrWhiteSpace(username))
+            {
+                meldingen.Add("Username is required.");
+                output = false;
+            }
+            else
+            {
+                if (username.Length < 4 || username.Length > 20)
+                {
+                    meldingen.Add("Username should be between 4 and 20 characters long.");
+                    output = false;
+                }
+
+                if (!username.All(char.IsLetterOrDigit))
+                {
+                    meldingen.Add("Username can only contain letters and digits.");
+                    output = false;
+                }
+            }
+
+            if (string.IsNullOrWhiteSpace(email))
+            {
+                meldingen.Add("Email is required.");
+                output = false;
+            }
+            else if (!email.Contains("@") || !email.Contains("."))
+            {
+                meldingen.Add("Invalid email format.");
+                output = false;
+            }
+
+            if (string.IsNullOrWhiteSpace(password))
+            {
+                meldingen.Add("Password is required.");
+                output = false;
+            }
+            else
+            {
+                if (password.Length < 8)
+                {
+                    meldingen.Add("Password should be at least 8 characters long and ");
+                    output = false;
+                }
+                else if (!password.Any(char.IsDigit) || !password.Any(char.IsUpper))
+                {
+                    meldingen.Add("Password should include at least one digit and one uppercase letter.");
+                    output = false;
+                }
+            }
+
+            foreach (string error in meldingen)
+            {
+                notification += $"\r\n{error}";
+            }
+
+            return output;
         }
     }
 }
